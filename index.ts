b2b02df5be39a1f2275d8fed9115511e35771618
Saravanan <>
2026-08-15 16:45:29 +0530

    initialise promclient and added metrics endpoint

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 import fastify from "fastify"
 import { db } from "./src/index.ts"
 import * as schema from './src/db/schema';
+import promClient from "prom-client";
+import { Client } from "pg";
 const app = fastify({
     logger: true
 })
 console.log(Bun.env.DATABASE_URL!);
 
-app.get('/health', (req, res) => {
+promClient.collectDefaultMetrics();
+
+app.get('/healthz', (req, res) => {
 
     res.status(200).send("Hi there !");
 })
 
+app.get('/metrics', async (req, res) => {
+    const metrics = await promClient.register.getMetricsAsJSON();
+
+    res.status(200).send(metrics);
+
+})
 app.listen({ port: Number(Bun.env.PORT) || 3000 });
